6b106ef0817ce511d45134b75cf0bb229c848dc5
Tesseracct <93708623+Tesseracct@users.noreply.github.com>
2021-11-16 14:26:19 +0100

    Added code to hide ratings

---
+++ solipsism.js
@@ -0,0 +1,8 @@
+document.body.style.border = "5px solid red";
+
+
+//document.getElementsByClassName("section ratings-histogram-chart").item(0).style.display = "none";
+
+setTimeout(function(){
+    document.getElementsByClassName("section ratings-histogram-chart").item(0).style.display = "none";
+}, 1)
