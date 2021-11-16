d76a03b84c9e003386a9a751e0d6f1c3ab367d6a
Tesseracct <93708623+Tesseracct@users.noreply.github.com>
2021-11-17 00:56:43 +0100

    Only hides ratings when not watched

--- code/solipsism.js
+++ code/solipsism.js
@@ -1,12 +1,18 @@
 document.getElementsByClassName("sidebar").item(0).style.display = "none";
 
 
-
 const targetNode = document.getElementsByClassName("sidebar").item(0);
 const config = { attributes: true, childList: true, subtree: true };
+
 const callback = function(mutationsList, observer) {
-    document.getElementsByClassName("section ratings-histogram-chart").item(0).style.display = "none";
-    document.getElementsByClassName("sidebar").item(0).style.display = "";
+    const watchedElement = document.getElementsByClassName("action-large -watch").item(0)
+    const watchedText = watchedElement.textContent
+
+    if(!(watchedText === "logged" || watchedText === "reviewed")){
+        document.getElementsByClassName("section ratings-histogram-chart").item(0).style.display = "none";
+    }
+
+    document.getElementsByClassName("sidebar").item(0).style.display = "block";
     observer.disconnect()
 }
 
